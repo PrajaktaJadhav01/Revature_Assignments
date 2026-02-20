8179d4f16dfab66948c7957765e801ebb747bdcf
Prajakta Jadhav <prajaktajadhav552@gmail.com>
2026-02-20 13:22:03 +0530

    Updated work

--- Day15/UnitTestingAdditionProgram.cs
+++ Day15/UnitTestingAdditionProgram.cs
@@ -2,39 +2,66 @@ using System;
 
 class Program
 {
-    static int Add(int a, int b)
+    static void Main()
     {
-        return a + b;
+        AddFunctionShouldReturn30ForInputs10And20();
+        AddFunctionShouldReturn40ForInputs20And20();
+        AddFunctionShouldReturn50ForInputs25And25();
     }
 
-    static void Main(string[] args)
+    static void AddFunctionShouldReturn30ForInputs10And20()
     {
-        int passedTests = 0;
-        int totalTests = 4;
+        // Arrange
+        var x = 10;
+        var y = 20;
+        var expectedResult = 30;
 
-        int result1 = Add(2, 3);
-        Console.WriteLine("2 + 3 = " + result1);
-        if (result1 == 5)
-            passedTests++;
+        // Act
+        var actualResult = Add(x, y);
 
-        int result2 = Add(10, 20);
-        Console.WriteLine("10 + 20 = " + result2);
-        if (result2 == 30)
-            passedTests++;
+        // Assert
+        Console.WriteLine($"Actual Result: {actualResult}, Expected Result: {expectedResult}");
 
-        int result3 = Add(-5, 5);
-        Console.WriteLine("-5 + 5 = " + result3);
-        if (result3 == 0)
-            passedTests++;
-
-        int result4 = Add(100, 200);
-        Console.WriteLine("100 + 200 = " + result4);
-        if (result4 == 300)
-            passedTests++;
-
-        if (passedTests == totalTests)
+        if (actualResult == expectedResult)
             Console.WriteLine("Test Passed");
         else
             Console.WriteLine("Test Failed");
+
+        Console.WriteLine();
+    }
+
+    static void AddFunctionShouldReturn40ForInputs20And20()
+    {
+        var expectedResult = 40;
+        var actualResult = Add(20, 20);
+
+        Console.WriteLine($"Actual Result: {actualResult}, Expected Result: {expectedResult}");
+
+        if (actualResult == expectedResult)
+            Console.WriteLine("Test Passed");
+        else
+            Console.WriteLine("Test Failed");
+
+        Console.WriteLine();
+    }
+
+    static void AddFunctionShouldReturn50ForInputs25And25()
+    {
+        var expectedResult = 50;
+        var actualResult = Add(25, 25);
+
+        Console.WriteLine($"Actual Result: {actualResult}, Expected Result: {expectedResult}");
+
+        if (actualResult == expectedResult)
+            Console.WriteLine("Test Passed");
+        else
+            Console.WriteLine("Test Failed");
+
+        Console.WriteLine();
+    }
+
+    static int Add(int a, int b)
+    {
+        return a + b;
     }
 }
