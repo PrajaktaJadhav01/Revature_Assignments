ebf4acee2154253a499edffde37a221cceef9ccc
Prajakta Jadhav <prajaktajadhav552@gmail.com>
2026-03-10 15:09:00 +0530

    Updated Work

---
+++ Day24/SingletonDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+
+// Register Singleton
+services.AddSingleton<DiSingleton>();
+
+// Build Service Provider
+var serviceProvider = services.BuildServiceProvider();
+
+// Get Singleton instances
+var diLogger1 = serviceProvider.GetRequiredService<DiSingleton>();
+var diLogger2 = serviceProvider.GetRequiredService<DiSingleton>();
+
+// Manual instance
+var loggerManual = new DiSingleton();
+
+// Print HashCodes
+Console.WriteLine("DI Singleton Instance 1: " + diLogger1.GetHashCode());
+Console.WriteLine("DI Singleton Instance 2: " + diLogger2.GetHashCode());
+Console.WriteLine("Manual Instance: " + loggerManual.GetHashCode());
+
+public class DiSingleton
+{
+    public int Value { get; set; }
+}
