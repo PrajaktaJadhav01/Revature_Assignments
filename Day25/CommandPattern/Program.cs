e4a0057049d63fe29e6eb4396a389bdf5b55c0e1
Prajakta Jadhav <prajaktajadhav552@gmail.com>
2026-03-13 10:20:17 +0530

    Updated Work

---
+++ Day25/CommandPattern/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+// Using Command Pattern
+ICommand createCustomerCommand = new CreateCustomerCommand();
+createCustomerCommand.Execute();
+
+// Direct Method Call (violates Open/Closed principle)
+CreateCustomer();
+
+void CreateCustomer()
+{
+    Console.WriteLine("Creating customer directly");
+}
+
+// Command Interface
+public interface ICommand
+{
+    void Execute();
+}
+
+// Concrete Command
+public class CreateCustomerCommand : ICommand
+{
+    public void Execute()
+    {
+        Console.WriteLine("Create Customer");
+    }
+}
